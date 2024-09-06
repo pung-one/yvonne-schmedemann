2f23c54a79103180683f495a6340adb68ffaae24
pung-one <p-ungerer@systemli.org>
2024-09-06 21:14:39 +0200

    preload font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,23 @@ import type { Metadata } from "next";
 import StyledComponentsRegistry from "@/lib/registry";
 import { GlobalStyles } from "@/globalStyles/global_styles";
 import { Layout } from "@/components/Layout";
+import ReactDOM from "react-dom";
 
 export const metadata: Metadata = {
   title: "Yvonne Schmedemann",
   description: "Photography by Yvonne Schmedemann",
   robots: { index: false, follow: false },
 };
+
+ReactDOM.preload("/fonts/logoFont.woff2", {
+  as: "font",
+  crossOrigin: "",
+});
+
+ReactDOM.preload("/fonts/IvarDisplay-Regular.woff2", {
+  as: "font",
+  crossOrigin: "",
+});
 
 export default function RootLayout({
   children,
